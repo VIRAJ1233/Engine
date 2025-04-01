f7721f207473181739b9e92b401a5223c8f11427
VIRAJ1233 <virajgayakwad.vg@gmail.com>
2025-04-01 13:53:28 +0530

    Initial commit

---
+++ Reverse/src/ReverseString.java
@@ -0,0 +1,16 @@
+
+public class ReverseString {
+	public static void main(String[] args) {
+		String str ="Akash";
+		char[] ch=str.toCharArray();
+		String reverse ="";
+		for(int i=ch.length-1;i>=0;i--)
+		{
+			reverse=reverse+ch[i];
+			System.out.println("Original String >>"+str);
+			System.out.println("Reverse String>>"+reverse);
+		}
+		
+	}
+
+}
